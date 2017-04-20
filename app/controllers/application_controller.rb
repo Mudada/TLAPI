46c39602c0476884ec9e66b324089f9c9dbf9875
Mudada <mudada@Mudadas-MacBook-Pro.local>
2017-04-20 11:03:45 +0200

    damn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def useless
-    render json: JSON.generate({categories: [{id: 1, name: 'streams'}, {id: 2, name: 'events'}]})
+    render json: JSON.generate({categories: [{id: 1, name: 'streams'}, {id: 2, name: 'levents'}, {id: 3, name: 'uevents'}]})
   end
 end
